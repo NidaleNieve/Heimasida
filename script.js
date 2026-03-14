6fb4c74f8958889c327f20b6a40389f20b40b4b7
NidaleNieve <nidalenieve@gmail.com>
2026-03-14 17:16:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,108 @@
+const translations = {
+    is: {}, // Verður fyllt sjálfkrafa út frá HTMLinu
+    en: {
+        subtitle: "Database and Software Developer",
+        intro: "I am a hardworking and diligent person with a strong interest in computers. I am very curious, responsible, solution-oriented, and work well independently.<br>Here you can find an overview of my projects and ways to get in touch.",
+        about_title: "About me",
+        about: 'I have worked on various web projects, ranging from games written in JavaScript to full-stack web applications. I enjoy creating things that work well...',
+        about_2: "I am always learning new things and looking for interesting projects.",
+        projects_title: "Projects",
+        hola: "Association of Spanish speakers in Iceland — Created a new website for Hola with Wordpress.",
+        gastroswipe: "Easily find restaurants with friends! Full Stack web-app built with Supabase and Next.js. <br>Web Programming II — Final Project.",
+        veff1_title: "Festivals in Iceland",
+        veff1: "Find festivals in Iceland near you. Built with Javascript and a JSON database. <br>Web Programming I — Final Project",
+        pacman: "Web Programming I — A Pac-Man like game written in JavaScript.",
+        vefh3: "Final Project — API-driven movie site built with Flask.",
+        vefh2: "Final Project — The Innovation Conference.",
+        contact_title: "Contact",
+        contact_text: "Want to work together or just say hi? Send me an email!",
+        contact_btn: "Send Email"
+    }
+};
+
+//Stilli upphafstungumálið
+let currentLang = "is";
+
+function switchLanguage() {
+    //easy switch
+    currentLang = currentLang === "is" ? "en" : "is";
+    //skiptir um texta á takkanum
+    document.getElementById("lang-switch").innerHTML = currentLang === "is" ? '<img src="assets/Flag_of_the_United_Kingdom.svg" alt="UK Flag">English</button>' : '<img src="assets/Flag_of_Iceland.svg" alt="Icelandic Flag">Íslenska</button>';
+    
+    //skiptir um texta í öllum elementum með data-translate attributinu
+    document.querySelectorAll("[data-translate]").forEach(element => {
+        const key = element.getAttribute("data-translate");
+        //ef að þýðingin er til þá skiptir það um tungumál
+        if (translations[currentLang][key]) {
+            element.innerHTML = translations[currentLang][key];
+        }
+    });
+}
+
+//Býr til observer object sem fylgist með ef að element byrtist í viewportið
+const scrolled = new IntersectionObserver(
+    //Bætir við visible klasa ef að elementið byrtist
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                scrolled.unobserve(entry.target); // hættir að fylgjast með
+            }
+        });
+    },
+    { threshold: 0.25 } //þarf að vera 25% í viewpotinu
+);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // Geymi allan textan sem íslenskt translation til þess að að þurfa ekki að skrifa textann tvisvar á íslensku
+    //fer í gegnum öll elements með data-translate attribute og geymir þau í is ef það er ekki til
+    document.querySelectorAll("[data-translate]").forEach(element => {
+        const key = element.getAttribute("data-translate");
+        if (!translations.is[key]) {
+            translations.is[key] = element.innerHTML.trim();
+        }
+    });
+
+    //hlustar eftir takkanum, ef svo þá keyrir switchLanguage fallið
+    const langBtn = document.getElementById("lang-switch");
+    if (langBtn) {
+        langBtn.addEventListener("click", switchLanguage);
+    }
+
+    // Set upp Vanilla Tilt fyrir öll cards sem nota það
+    // Profile picture
+    VanillaTilt.init(document.querySelectorAll(".profile-image-wrapper"), {
+        scale: 1.04,
+        perspective: 800,
+        max: 8,
+        speed: 2000,
+        glare: true,
+        "max-glare": 0.15,
+    });
+
+    // About card
+    VanillaTilt.init(document.querySelectorAll(".about-card"), {
+        scale: 1.008,
+        perspective: 1200,
+        max: 3,
+        speed: 4000,
+        glare: true,
+        "max-glare": 0.1,
+    });
+
+    //Project cards
+    VanillaTilt.init(document.querySelectorAll(".project-card"), {
+        scale: 1.015,
+        perspective: 1000,
+        max: 4,
+        speed: 3000,
+        glare: true,
+        "max-glare": 0.18,
+    });
+
+    //Stilli upp observer fyrir öll section elements
+    const sections = document.querySelectorAll("Section");
+    sections.forEach((section) => scrolled.observe(section));
+});
